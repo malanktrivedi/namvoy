29c4ce8fc363900c2341326f7708296dea2b7e1e
Sai Trivedi <82054476+malanktrivedi@users.noreply.github.com>
2026-09-05 04:32:52 +0530

    Add MySQLi database connection

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/config.php';
+
+function db(): mysqli
+{
+    static $connection = null;
+    if ($connection instanceof mysqli) {
+        return $connection;
+    }
+
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $connection->set_charset('utf8mb4');
+    return $connection;
+}
