3aa133aa6e6a40eaa24f1e89425db750706e161c
Sai Trivedi <82054476+malanktrivedi@users.noreply.github.com>
2026-09-05 04:43:05 +0530

    Add traveler bookings navigation

--- includes/header.php
+++ includes/header.php
@@ -20,7 +20,7 @@ $user = current_user();
       <ul class="navbar-nav ms-auto align-items-lg-center gap-lg-2">
         <li class="nav-item"><a class="nav-link" href="/trip-planner.php">Build My Trip</a></li>
         <?php if ($user): ?>
-          <?php if ($user['role'] === 'traveler'): ?><li class="nav-item"><a class="nav-link" href="/account/dashboard.php">Dashboard</a></li><?php endif; ?>
+          <?php if ($user['role'] === 'traveler'): ?><li class="nav-item"><a class="nav-link" href="/account/dashboard.php">Dashboard</a></li><li class="nav-item"><a class="nav-link" href="/account/bookings.php">Bookings</a></li><?php endif; ?>
           <?php if ($user['role'] === 'provider'): ?><li class="nav-item"><a class="nav-link" href="/provider/dashboard.php">Provider Dashboard</a></li><?php endif; ?>
           <li class="nav-item"><a class="btn btn-dark btn-sm" href="/logout.php">Logout</a></li>
         <?php else: ?>
